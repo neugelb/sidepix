af2ad727ea68b249f37fcd3c7e6bc8466b21d470
Dario Aprea <dario@neugelb.com>
2023-02-26 12:58:21 +0100

    Fix makeGetPictureData dropping className

--- lib/core/getPictureDataClientSide.ts
+++ lib/core/getPictureDataClientSide.ts
@@ -56,6 +56,7 @@ export function makeGetPictureData(
     );
 
     return {
+      className: props.className,
       sources: processedSources,
       fallback: createFileUrl(fallback),
       alt: props.alt,
